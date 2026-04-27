dd8c0bb3ce6c6facb01c11a572864cc94e7f4d7d
Crystal <crystalleefetterman@gmail.com>
2026-04-27 13:48:19 -0700

    logo

--- woonuxt_base/app/components/generalElements/Logo.vue
+++ woonuxt_base/app/components/generalElements/Logo.vue
@@ -4,15 +4,14 @@ const runtimeConfig = useRuntimeConfig();
 const img = useImage();
 
 const logoUrl = runtimeConfig?.public?.LOGO ? img(runtimeConfig?.public?.LOGO) : null;
-const faviconUrl = '/logo.svg';
+const faviconUrl = '/liliesofthefield.webp';
 </script>
 
 <template>
   <NuxtLink to="/" class="inline-flex items-center gap-2">
     <img v-if="logoUrl" :src="logoUrl" alt="Logo" class="object-contain h-10" />
     <div v-else class="flex items-center gap-2 text-lg font-bold">
-      <img :src="faviconUrl" alt="Logo" width="32" height="32" />
-      <span>{{ siteName }}</span>
+      <img :src="faviconUrl" alt="Logo" />
     </div>
   </NuxtLink>
 </template>
