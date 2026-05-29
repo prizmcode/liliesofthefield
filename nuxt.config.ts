0ab06a7228b6b1ae5252dce725c387f0d4091159
Crystal <crystalleefetterman@gmail.com>
2026-05-29 10:55:44 -0700

    installed gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,12 @@ export default defineNuxtConfig({
 
  components: [{ path: "./app/components", pathPrefix: false, priority: 10 }],
 
+ modules: ["nuxt-gtag"],
+
+ gtag: {
+  id: "G-L4PCRT3MWR",
+ },
+
  /**
   * Depending on your servers capabilities, you may need to adjust the following settings.
   * It will affect the build time but also increase the reliability of the build process.
